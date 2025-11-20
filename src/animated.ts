90c34a44611a26036f2449bbeb5403b202d3ae30
Chuanqi Sun <chuanqisun-no-reply@gmail.com>
2025-11-20 18:04:40 -0500

    remove console log

--- src/animated.ts
+++ src/animated.ts
@@ -182,12 +182,10 @@ function animate() {
 
     if (isWalkingIn.value && topDownCamera) {
       topDownCamera.position.z -= walkInSpeed * mixerUpdateDelta;
-      console.log("Top-down camera Z position:", topDownCamera.position.z);
     }
 
     if (isWalkingOut.value && topDownCamera) {
       topDownCamera.position.z -= walkOutSpeed * mixerUpdateDelta;
-      console.log("Top-down camera Z position:", topDownCamera.position.z);
     }
 
     // Update weights
